afbb1f6e8228dab9040f4aee91e85cc8dde10415
bachhh <bachhh@users.noreply.github.com>
2022-07-10 16:37:52 +0700

    temp

--- src/utils.rs
+++ src/utils.rs
@@ -9,32 +9,71 @@ pub type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 // Multiple insertions will not change the number and only the first insert is kept.
 pub struct HashLinkedMap<T> {
     hash_map: HashMap<T, u32>,
-    list: Vec<T>,
+    list: Vec<Option<T>>,
     head: usize,
     tail: usize,
     length: usize,
-    capacity: usize,
 }
+
+const MIN_DENSITY: f64 = 0.5;
 
 impl<T> HashLinkedMap<T> {
     pub fn push(&mut self, entry: T) {
-        if (self.length > 0 && self.tail == self.head) {
-            if self.length == self.capacity {
-                self.expand();
+        if self.length > 0 && self.tail == self.head {
+            if self.length == self.capacity() {
+                self.extend();
             } else {
                 self.pack();
             }
         }
-        self.list[self.tail] = entry;
-        self.tail = self.tail + 1 % self.capacity;
+        self.list[self.tail] = Some(entry);
+        self.tail = self.tail + 1 % self.capacity();
         self.length += 1;
+    }
+
+    fn capacity(&self) -> usize {
+        return self.list.capacity();
     }
 
     // pack is called when the density ( length / capacity ) went below certain level.
     // then the entire array is repacked by shifting all elements close together.
-    fn pack(&mut self) {}
+    fn pack(&mut self) {
+        let mut write = self.head.clone();
+        let mut read = write;
+        let mut count = 0;
+        while count < self.length {
+            if let None = self.list[write] {
+                // advance read until we see a none empty slot
+                if read <= write {
+                    read = (write + 1) % self.capacity();
+                }
+                while let None = self.list[read] {
+                    read = (read + 1) % self.capacity();
+                }
 
-    // expand is called when push() causes length to exceed the current capacity.
+                self.list.swap(read, write);
+            }
+            write = (write + 1) % self.capacity();
+            count += 1;
+        }
+    }
+
+    // extend is called when push() causes length to exceed the current capacity.
     // If the array is packable, it should be packed first before considering for expansion.
-    fn expand(&mut self) {}
+    // after resize, density (length/capacity) should be at least avg(min_density, max_density)
+    fn extend(&mut self) {
+        let old_capacity = self.capacity();
+        self.list
+            .reserve(((old_capacity as f64) / ((1.0 + MIN_DENSITY) * 2.0)) as usize);
+        // splice the "front" of array to the tail of array
+
+        let old_tail = self.tail;
+        self.tail = old_capacity;
+        for i in 0..old_tail {
+            if let Some(entry) = self.list[i] {
+                self.push(entry);
+                self.list[i] = None;
+            }
+        }
+    }
 }
