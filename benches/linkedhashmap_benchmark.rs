2b9c971a2f0a2264cf5ec5b273f168128f76e503
bachhh <bachhh@users.noreply.github.com>
2022-08-09 15:47:15 +0700

    temp

--- benches/linkedhashmap_benchmark.rs
+++ benches/linkedhashmap_benchmark.rs
@@ -8,52 +8,81 @@ fn increment_list(mut list: LinkedList<i64>) {
     }
 }
 
-// fn increment_vec(mut vec: Vec<i64>) {}
+fn increment_vec(mut vec: Vec<i64>) {
+    for element in vec.iter_mut() {
+        *element += 1;
+    }
+}
 
 fn bench(c: &mut Criterion) {
     // test param = array & linkedlist of increasingly larger size, for array we will consider
     // 4 density factors: 25% 50% 75% 100%
     // test case involve exponentials of 2 up to 2^10
-    for exp in 1..15 {
-        let mut list = LinkedList::<i64>::new();
-        for i in 1..2 ^ exp {
-            list.push_back(i);
-        }
+    let mut group = c.benchmark_group("My Group");
+    for exp in 1..17 {
+        let x = 1 << exp;
 
-        // // create 4 vectors of density 25, 50, 75, 100
-        // let mut v25 = vec![0; (2 ^ exp * 4) as usize];
-        // for i in 1..2 ^ exp * 4 {
-        //     if i % 4 == 0 {
-        //         v25[(i - 1) as usize] = i;
-        //     }
-        // }
-
-        // let mut v50 = vec![0; (2 ^ exp * 2) as usize];
-        // for i in 1..2 ^ exp * 4 {
-        //     if i % 2 != 0 {
-        //         v50[(i - 1) as usize] = i;
-        //     }
-        // }
-
-        // let mut v75 = vec![0; (2 ^ exp * 2) as usize];
-        // for i in 1..2 ^ exp * 4 {
-        //     if i % 4 != 0 {
-        //         v75[(i - 1) as usize] = i;
-        //     }
-        // }
-
-        // let mut v100 = vec![0; (2 ^ exp * 2) as usize];
-        // for i in 1..2 ^ exp * 4 {
-        //     v100[(i - 1) as usize] = i;
-        // }
-
-        c.bench_function(&format!("llist {}", 2 ^ exp).to_string(), move |b| {
-            b.iter_batched(|| list.clone(), increment_list, BatchSize::SmallInput);
+        group.bench_function(&format!("llist {}", x).to_string(), move |b| {
+            b.iter_batched(
+                || {
+                    let mut list = LinkedList::<i64>::new();
+                    for i in 1..x {
+                        list.push_back(i);
+                    }
+                    list
+                },
+                increment_list,
+                BatchSize::SmallInput,
+            );
         });
 
-        // c.bench_function(&format!("array {}-25", 2 ^ exp).to_string(), |b| {
-        //     b.iter(|| increment_vec(black_box(v25)))
-        // });
+        group.bench_function(&format!("array {}-25", x).to_string(), move |b| {
+            b.iter_batched(
+                || {
+                    let mut v25: Vec<i64> = vec![0; (x * 2) as usize];
+                    for (i, v) in v25.iter_mut().enumerate() {
+                        if i % 4 == 0 {
+                            *v = i as i64;
+                        }
+                    }
+                    v25
+                },
+                increment_vec,
+                BatchSize::SmallInput,
+            );
+        });
+
+        //         group.bench_function(&format!("array {}-50", x).to_string(), move |b| {
+        //             b.iter_batched(
+        //                 || {
+        //                     let mut v50: Vec<i64> = vec![0; (x * 2) as usize];
+        //                     for (i, v) in v50.iter_mut().enumerate() {
+        //                         if i % 2 != 0 {
+        //                             *v = i as i64;
+        //                         }
+        //                     }
+        //                     v50
+        //                 },
+        //                 increment_vec,
+        //                 BatchSize::SmallInput,
+        //             );
+        //         });
+
+        //         group.bench_function(&format!("array {}-75", x).to_string(), move |b| {
+        //             b.iter_batched(
+        //                 || {
+        //                     let mut v75: Vec<i64> = vec![0; (x * 2) as usize];
+        //                     for (i, v) in v75.iter_mut().enumerate() {
+        //                         if i % 4 != 0 {
+        //                             *v = i as i64;
+        //                         }
+        //                     }
+        //                     v75
+        //                 },
+        //                 increment_vec,
+        //                 BatchSize::SmallInput,
+        //             );
+        //         });
     }
 }
 
